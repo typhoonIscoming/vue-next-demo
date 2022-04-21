4134da7b1adaabcee322687469dc9130837c867c
typhoonIscoming <850525344@qq.com>
2022-04-21 21:28:21 +0800

    mod: changeImageColor

---
+++ src/components/ChangeImageColor.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="ChangeImageColor">
+        <img src="~@/assets/changeImageColor/iwatch.png">
+        <div class="nav">
+            <span class="btn one active"></span>
+            <i class="one"></i>
+            <span class="btn two"></span>
+            <i class="two"></i>
+            <span class="btn three"></span>
+            <i class="three"></i>
+            <span class="btn four"></span>
+            <i class="four"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TseChangeImageColor',
+    mounted() {
+        const span = document.querySelectorAll('.btn');
+        span.forEach((el) => {
+            el.addEventListener('click', function() {
+                const active = document.querySelectorAll('.btn.active');
+                active.forEach((item) => {
+                    item.classList.remove('active');
+                })
+                el.classList.add('active')
+            })
+        })
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.ChangeImageColor{
+    width: 300px;
+    height: 200px;
+    position: relative;
+    display: flex;
+    justify-content: flex-end;
+    img{
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: contain;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+    .nav{
+        display: flex;
+        flex-direction: column;
+        
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+    .btn{
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        margin-top: 10px;
+        cursor: pointer;
+        z-index: 2;
+        &.active{
+            border: 2px solid #000;
+            transform: scale(1.2);
+            &+i{
+                position: absolute;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                mix-blend-mode: hue;
+            }
+        }
+        &.one, &~i.one{
+            background: #F1D56B;
+        }
+        &.two, &~i.two{
+            background: #BCCCBB;
+        }
+        &.three, &~i.three{
+            background: #617165;
+        }
+        &.four, &~i.four{
+            background: #F36154;
+        }
+    }
+}
+</style>
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,17 +1,29 @@
 <template>
     <div class="Dashboard">
+        <div class="row">
+            <TseChangeImageColor />
+        </div>
         Dashboard
     </div>
 </template>
 
 <script>
+import TseChangeImageColor from '@/components/ChangeImageColor.vue';
+
 export default {
     name: 'Dashboard',
+    components: {
+        TseChangeImageColor,
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 .Dashboard{
     height: 100vh;
+    .row{
+        display: flex;
+        padding: 10px;
+    }
 }
 </style>
