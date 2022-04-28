8ef0d27e8851caa736f8f644d7fca382ea25cc6f
typhoonIscoming <850525344@qq.com>
2022-04-28 12:04:51 +0800

    mod: 更新icon

---
+++ src/components/LikeEffect.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="LikeEffect">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TseLikeEffect',
+}
+</script>
+
+<style lang="scss" scoped>
+.LikeEffect{
+    
+}
+</style>
