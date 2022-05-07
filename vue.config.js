d1042f6f077156651535f840e2315acfd586bf08
typhoonIscoming <850525344@qq.com>
2022-05-07 16:27:23 +0800

    mod: 添加post-css

--- vue.config.js
+++ vue.config.js
@@ -18,6 +18,24 @@ module.exports = defineConfig({
     productionSourceMap: false,
     outputDir: 'dist',
     assetsDir: 'static',
+    // css: {
+    //     loaderOptions: {
+    //         postcss: {
+    //             postcssOptions: {
+    //                 plugins: [
+    //                     require('postcss-pxtorem')({
+    //                         rootValue: 100, // 换算的基数
+    //                         // 忽略转换正则匹配项。插件会转化所有的样式的px。比如引入了三方UI，也会被转化。目前我使用 selectorBlackList字段，来过滤
+    //                         // 如果个别地方不想转化px。可以简单的使用大写的 PX 或 Px 。
+    //                         selectorBlackList: [".van-"],
+    //                         propList: ["*"],
+    //                         exclude: /node_modules/,
+    //                     }),
+    //                 ]
+    //             }
+    //         }
+    //     },
+    // },
     configureWebpack: {
         name: projectName,
         resolve: {
@@ -51,7 +69,7 @@ module.exports = defineConfig({
                     to: 'static'
                 }]
             }),
-        ]
+        ],
     },
     chainWebpack: config => {
         config.module
