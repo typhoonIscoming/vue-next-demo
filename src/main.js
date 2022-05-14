d9aecbe7ac87e281a8d9c258b460f6df47127dc1
typhoonIscoming <850525344@qq.com>
2022-05-15 01:05:21 +0800

    mod: 引入element  button

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+
 import './assets/icons/iconfont.css';
+
+import 'element-plus/es/components/button/style/css'
 
 const app = createApp(App);
 app.use(router);
--- src/views/Manager.vue
+++ src/views/Manager.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="Manager">
+        <ElButton type="primary">ElButton</ElButton>
         Manager
     </div>
 </template>
 
 <script>
+import { ElButton } from 'element-plus';
+
 export default {
     name: 'Manager',
+    components: {
+        ElButton,
+    },
 }
 </script>
 
