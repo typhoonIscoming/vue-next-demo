a85be2969500e3071c7803ab94854f46cc7f5b1f
typhoonIscoming <850525344@qq.com>
2022-10-24 15:41:52 +0800

    dynamicComponent

--- src/views/Manager.vue
+++ src/views/Manager.vue
@@ -8,14 +8,26 @@
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
 import { ElButton } from 'element-plus';
 import ScaleInputText from '@/components/ScaleInputText.vue';
-import PricticeStore from '@/modules/practiceStore.vue';
+// import PricticeStore from '@/modules/practiceStore.vue';
 
 export default {
     name: 'Manager',
     components: {
-        ElButton, ScaleInputText, PricticeStore,
+        ElButton, ScaleInputText,
+        // 可以拆分模块打包
+        PricticeStore: defineAsyncComponent({
+            loader: () => new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(import('@/modules/practiceStore.vue'))
+                }, 2000)
+            }),
+            loadingComponent: 'loading',
+            errorComponent: 'error',
+            timeout: 3000,
+        }),
     },
     mounted() {
         console.log('===', this.$refs.prictiseStore)
